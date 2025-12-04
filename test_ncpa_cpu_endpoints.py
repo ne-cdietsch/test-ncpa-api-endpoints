124488c37101d7310f85b4fe5795e2814ac77940
Coleman Dietsch <ne.cdietsch@gmail.com>
2025-12-04 14:28:46 -0800

    Updating test names to be less redundant

--- test_ncpa_cpu_endpoints.py
+++ test_ncpa_cpu_endpoints.py
@@ -13,14 +13,14 @@ def test_cpu_endpoint():
     assert response.status_code == 200
     assert isinstance(response.json(), dict)
 
-def test_cpu_percent_endpoint():
+def test_cpu_percent():
     url = f"{BASE_URL}/api/cpu/percent?token={API_TOKEN}"
     response = requests.get(url, verify=False)
     logger.info(response.json())
     assert response.status_code == 200
     assert isinstance(response.json(), dict)
 
-def test_cpu_percent_as_check_endpoint():
+def test_cpu_percent_as_check():
     url = f"{BASE_URL}/api/cpu/percent?token={API_TOKEN}&warning=80&critical=90&check=true"
     response = requests.get(url, verify=False)
     logger.info(response.json())
--- test_ncpa_disk_endpoints.py
+++ test_ncpa_disk_endpoints.py
@@ -13,14 +13,14 @@ def test_disk_endpoint():
     assert response.status_code == 200
     assert isinstance(response.json(), dict)
 
-def test_disk_physical_endpoint():
+def test_disk_physical():
     url = f"{BASE_URL}/api/disk/physical?token={API_TOKEN}"
     response = requests.get(url, verify=False)
     logger.info(response.json())
     assert response.status_code == 200
     assert isinstance(response.json(), dict)
 
-def test_disk_mount_endpoint():
+def test_disk_mount():
     url = f"{BASE_URL}/api/disk/mount?token={API_TOKEN}"
     response = requests.get(url, verify=False)
     logger.info(response.json())
@@ -29,15 +29,15 @@ def test_disk_mount_endpoint():
 
 # The logical device names will vary based on OS and configuration
 if SYSTEM_TYPE != 'windows':
-    def test_disk_logical_used_percent_endpoint():
+    def test_disk_logical_root_used_percent():
         url = f"{BASE_URL}/api/disk/logical/|/used_percent?token={API_TOKEN}"
         response = requests.get(url, verify=False)
         logger.info(response.json())
         assert response.status_code == 200
         assert isinstance(response.json(), dict)
 else:
-    def test_disk_logical_c_endpoint():
-        url = f"{BASE_URL}/api/disk/logical/C|/used_percent?token={API_TOKEN}"
+    def test_disk_logical_c_used_percent():
+        url = f"{BASE_URL}/api/disk/logical/C:|/used_percent?token={API_TOKEN}"
         response = requests.get(url, verify=False)
         logger.info(response.json())
         assert response.status_code == 200
