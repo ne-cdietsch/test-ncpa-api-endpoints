f722543bf9b2fc7b2fb7c536872589b509ab533c
Coleman Dietsch <ne.cdietsch@gmail.com>
2025-12-08 11:28:12 -0800

    Adding updated cpu tests

--- test_ncpa_cpu_endpoints.py
+++ test_ncpa_cpu_endpoints.py
@@ -5,25 +5,12 @@ from ncpa_config import *
 
 
 # Test CPU endpoint
-def test_cpu_endpoint():
-    url = f"{BASE_URL}/api/cpu?token={API_TOKEN}"
-    response = requests.get(url, verify=False)
-    logger.info(response.json())
-    assert response.status_code == 200
-    assert isinstance(response.json(), dict)
+ENDPOINT_DATA = [
+    ("cpu"),
+    ("cpu/percent"),
+    ("cpu/percent&warning=80&critical=90&check=true"),
+]
 
-def test_cpu_percent():
-    url = f"{BASE_URL}/api/cpu/percent?token={API_TOKEN}"
-    response = requests.get(url, verify=False)
-    logger.info(response.json())
-    assert response.status_code == 200
-    assert isinstance(response.json(), dict)
-
-def test_cpu_percent_as_check():
-    url = f"{BASE_URL}/api/cpu/percent?token={API_TOKEN}&warning=80&critical=90&check=true"
-    response = requests.get(url, verify=False)
-    logger.info(response.json())
-    assert response.status_code == 200
-    assert isinstance(response.json(), dict)
-    assert isinstance(response.json()["returncode"], int)
-    assert isinstance(response.json()["stdout"], str)
+@pytest.mark.parametrize("endpoint", ENDPOINT_DATA)
+def test_cpu_endpoints(endpoint):
+    get_endpoint(endpoint)
