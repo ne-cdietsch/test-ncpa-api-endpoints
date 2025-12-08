75dc69be88ed07dc63ec78b1968081f6180fad29
Coleman Dietsch <ne.cdietsch@gmail.com>
2025-12-08 11:25:09 -0800

    Updating test connection and cpu tests

--- test_ncpa_connection.py
+++ test_ncpa_connection.py
@@ -9,7 +9,7 @@ ENDPOINT_DATA = [
 ]
 
 def test_connection():
-    url = f"{BASE_URL}/testconnect?token={API_TOKEN}"
+    url = f"{BASE_URL.replace("api/", "")}/testconnect?token={API_TOKEN}"
     response = requests.get(url, verify=False)
     logger.info(response.json())
     assert response.status_code == 200
