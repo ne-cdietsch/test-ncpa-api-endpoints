630480176b83f693033886189b430d5bff480909
Coleman Dietsch <ne.cdietsch@gmail.com>
2026-01-12 09:35:35 -0800

    feat: update tests

--- test_ncpa_services_endpoints.py
+++ test_ncpa_services_endpoints.py
@@ -3,22 +3,22 @@ import pytest
 from ncpa_config import *
 
 
-# Test processes endpoint
+# Test services endpoint
 #
 # List of endpoints to test
 ENDPOINT_DATA = [
-    ("processes"),
+    ("services"),
 ]
 
 # List of endpoints to test as checks
 CHECK_DATA = [
-    ("processes"),
+    ("services"),
 ]
 
 @pytest.mark.parametrize("endpoint", ENDPOINT_DATA)
-def test_processes_endpoints(endpoint):
+def test_services_endpoints(endpoint):
     get_endpoint(endpoint)
 
 @pytest.mark.parametrize("endpoint", CHECK_DATA)
-def test_processes_endpoints_as_checks(endpoint):
+def test_services_endpoints_as_checks(endpoint):
     get_endpoint_as_check(endpoint)
