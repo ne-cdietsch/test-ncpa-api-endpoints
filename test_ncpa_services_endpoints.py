498afe6b486e2e563521798abb1750315c69e2ca
Coleman Dietsch <ne.cdietsch@gmail.com>
2026-01-12 09:33:48 -0800

    feat: add tests for services endpoint

---
+++ test_ncpa_services_endpoints.py
@@ -0,0 +1,24 @@
+import pytest
+# Import ncpa_config module to access configuration and helper functions
+from ncpa_config import *
+
+
+# Test processes endpoint
+#
+# List of endpoints to test
+ENDPOINT_DATA = [
+    ("processes"),
+]
+
+# List of endpoints to test as checks
+CHECK_DATA = [
+    ("processes"),
+]
+
+@pytest.mark.parametrize("endpoint", ENDPOINT_DATA)
+def test_processes_endpoints(endpoint):
+    get_endpoint(endpoint)
+
+@pytest.mark.parametrize("endpoint", CHECK_DATA)
+def test_processes_endpoints_as_checks(endpoint):
+    get_endpoint_as_check(endpoint)
