dd67595cf9d1ed58dca1da9a6ef4f8f8d4424058
Coleman Dietsch <ne.cdietsch@gmail.com>
2026-01-12 10:45:42 -0800

    fix: update services tests with filters

--- test_ncpa_services_endpoints.py
+++ test_ncpa_services_endpoints.py
@@ -10,29 +10,23 @@ ENDPOINT_DATA = [
     ("services"),
 ]
 
+ENDPOINT_DATA_FILTERED = [
+    ("services", "&services=ncpa&match=search"),
+]
+
 # List of endpoints to test as checks
-CHECK_DATA = [
-    ("services"),
+CHECK_DATA_FILTERED = [
+    ("services", "&services=ncpa&match=search"),
 ]
 
 @pytest.mark.parametrize("endpoint", ENDPOINT_DATA)
 def test_services_endpoints(endpoint):
     get_endpoint(endpoint)
 
-@pytest.mark.parametrize("endpoint", CHECK_DATA)
-def test_services_endpoints_as_checks(endpoint):
-    get_endpoint_as_check(endpoint)
+@pytest.mark.parametrize("endpoint,filters", ENDPOINT_DATA_FILTERED)
+def test_services_endpoints_filtered(endpoint, filters):
+    get_endpoint_filtered(endpoint, filters)
 
-# Additional filter configurations
-FILTERS = [
-    "&services=ncpa",
-    "&match=search",
-]
-
-@pytest.mark.parametrize("endpoint", ENDPOINT_DATA)
-def test_services_endpoints_filtered(endpoint):
-    get_endpoint(endpoint)
-
-@pytest.mark.parametrize("endpoint", CHECK_DATA)
-def test_services_endpoints_as_checks_filtered(endpoint):
-    get_endpoint_as_check(endpoint)
+@pytest.mark.parametrize("endpoint,filters", CHECK_DATA_FILTERED)
+def test_services_endpoints_as_checks_filtered(endpoint, filters):
+    get_endpoint_as_check_filtered(endpoint, filters)
